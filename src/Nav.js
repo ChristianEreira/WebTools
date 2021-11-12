3535a170cfb26c91dace9a0de45c6a31bfc53781
Christian Ereira <contact@chrise.dev>
2021-11-12 01:02:06 +0000

    Add basic mobile menu

--- src/Nav.js
+++ src/Nav.js
@@ -1,10 +1,24 @@
 import './App.css';
-import { Link, NavLink, useLocation } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
+import { useState, useEffect } from 'react';
 
 function Nav() {
-  console.log(useLocation());
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  useEffect(() => {
+    window.addEventListener('resize', () => setWindowWidth(window.innerWidth));
+  }, []);
+
+  function toggleMenu(e) {
+    e.preventDefault();
+    setMenuOpen(!menuOpen);
+  }
+
+  let isMobile = windowWidth < 800;
   return (
-    <nav>
+    <div>
+    <nav className={isMobile ? ("mobileNav" + (menuOpen ? "" : " hidden")) : ""}>
       <h1><Link to="/">&lt;WebTools/&gt;</Link></h1>
       <ul>
         <li>Button Generator</li>
@@ -13,6 +27,8 @@ function Nav() {
         </NavLink>
       </ul>
     </nav>
+    {isMobile && <div id="menuBtn" onClick={toggleMenu}>Menu</div>}
+    </div>
   );
 }
 
