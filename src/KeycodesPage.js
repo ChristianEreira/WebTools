f6386b148afee79b0b049d44ea7ad671cce72d5e
Christian Ereira <contact@chrise.dev>
2021-11-19 17:06:33 +0000

    Add styling for keycode table

--- src/KeycodesPage.js
+++ src/KeycodesPage.js
@@ -5,6 +5,19 @@ function KeycodesPage() {
     <div className="centerVert">
       <p className="section">Press any key to get the JavaScript keycode</p>
       <p className="bigNumber">33</p>
+      <div id="keycodeTable">
+        <div id="keycodeTableLeft">
+          <p>event.key</p>
+          <p>event.code</p>
+          <p>event.which</p>
+        </div>
+        <div id="keycodeTableSeperator"></div>
+        <div>
+          <p>...</p>
+          <p>...</p>
+          <p>...</p>
+        </div>
+      </div>
     </div>
   );
 }
