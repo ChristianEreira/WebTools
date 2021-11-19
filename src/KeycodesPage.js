c5456203717c48448fdcff8d2def3f62306e5e7a
Christian Ereira <contact@chrise.dev>
2021-11-19 17:23:01 +0000

    Add functionality

--- src/KeycodesPage.js
+++ src/KeycodesPage.js
@@ -1,10 +1,20 @@
 import './App.css';
+import { useState, useEffect } from 'react';
 
 function KeycodesPage() {
+  const [keyEvent, setKeyEvent] = useState({ key: "...", code: "...", which: "..." });
+
+  useEffect(() => {
+    window.addEventListener('keydown', (e) => {
+      setKeyEvent(e);
+      console.log(e);
+    });
+  }, []);
+
   return (
     <div className="centerVert">
       <p className="section">Press any key to get the JavaScript keycode</p>
-      <p className="bigNumber">33</p>
+      <p className="bigNumber">{keyEvent.which}</p>
       <div id="keycodeTable">
         <div id="keycodeTableLeft">
           <p>event.key</p>
@@ -13,9 +23,9 @@ function KeycodesPage() {
         </div>
         <div id="keycodeTableSeperator"></div>
         <div>
-          <p>...</p>
-          <p>...</p>
-          <p>...</p>
+          <p>{keyEvent.key == " " ? '" "' : keyEvent.key}</p>
+          <p>{keyEvent.code}</p>
+          <p>{keyEvent.which}</p>
         </div>
       </div>
     </div>
