8d6e66a6cbe7b42751c019c67dd34b85cf494026
Christian Ereira <contact@chrise.dev>
2021-11-19 03:54:13 +0000

    Add basic design for elements

--- src/KeycodesPage.js
+++ src/KeycodesPage.js
@@ -2,7 +2,10 @@ import './App.css';
 
 function KeycodesPage() {
   return (
-    <h3>JS Keycodes</h3>
+    <div className="centerVert">
+      <p className="section">Press any key to get the JavaScript keycode</p>
+      <p className="bigNumber">33</p>
+    </div>
   );
 }
 
